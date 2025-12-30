1e921e9dcf974afcfdd906eddf8f3d24b4c730f3
Ansh Rastogi <thisisanshrastogi@gmail.com>
2025-12-30 17:52:07 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
     return [
       {
         source: "/api/v1/:path*",
-        // destination: "http://13.48.48.207:/api/v1/:path*",
-        destination: "http://localhost:2707/api/v1/:path*",
+        destination: "https://api.byteapp.tech/api/v1/:path*",
+        // destination: "http://localhost:2707/api/v1/:path*",
       },
     ];
   },
