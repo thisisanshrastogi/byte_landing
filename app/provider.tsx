007566c5f9b2e106203f278f422e76c4d3e12fa7
Ansh Rastogi <thisisanshrastogi@gmail.com>
2026-01-23 13:15:35 +0530

    dep

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,11 +1,11 @@
-"use client";
+// "use client";
 
-import { GoogleOAuthProvider } from "@react-oauth/google";
+// import { GoogleOAuthProvider } from "@react-oauth/google";
 
-export default function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
-      {children}
-    </GoogleOAuthProvider>
-  );
-}
+// export default function Providers({ children }: { children: React.ReactNode }) {
+//   return (
+//     <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
+//       {children}
+//     </GoogleOAuthProvider>
+//   );
+// }
